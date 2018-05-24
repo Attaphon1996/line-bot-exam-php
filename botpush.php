6d81900414d856af6adee03752eb93f299246b3e
attaphon wongwikitkan (boat) <boatbright00@gmail.com>
2018-05-24 17:41:41 +0700

    asdsad

--- botpush.php
+++ botpush.php
@@ -1,32 +1,31 @@
 <?php
 
-
-
 require "vendor/autoload.php";
+if (isset($_GET['masg'])) {
+	$data = $_GET['masg'];
+	$access_token = '1xRbg9ZC/ZDa9yTUfcsnca6+776kRaRV1SYOHEHLVsAdGVroKwefKlR48C1O/hVTYQtcDqujTEyK8McaiqaoudaL2BsAUxbf91jJkxnyrTYlDTB8hSBtAMm/uASSo6WDt9KbKjPl0ZbI9fpU6mNqMgdB04t89/1O/w1cDnyilFU=';
+	$channelSecret = '11c8e5f96b7d59d300b100092b5834d3';
 
-$access_token = '1xRbg9ZC/ZDa9yTUfcsnca6+776kRaRV1SYOHEHLVsAdGVroKwefKlR48C1O/hVTYQtcDqujTEyK8McaiqaoudaL2BsAUxbf91jJkxnyrTYlDTB8hSBtAMm/uASSo6WDt9KbKjPl0ZbI9fpU6mNqMgdB04t89/1O/w1cDnyilFU=';
-$channelSecret = '11c8e5f96b7d59d300b100092b5834d3';
+	$pushID = ['U1b6de0ae915180340652adccba3e084f','U20b47d150d50b896bbbf53bddbfa601a'];
 
-$pushID = ['U1b6de0ae915180340652adccba3e084f','U20b47d150d50b896bbbf53bddbfa601a'];
+	$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+	$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+	$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($data);
+	$file = fopen("vardump.txt", "r");
+	$members = array();
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
-$file = fopen("vardump.txt", "r");
-$members = array();
+	while (!feof($file)) {
+	   $members[] = fgets($file);
+	}
 
-while (!feof($file)) {
-   $members[] = fgets($file);
+	fclose($file);
+	$userid = $members;
+	for($i=1;$i<count($userid);$i++){
+		$response = $bot->pushMessage($userid[$i], $textMessageBuilder);
+	}
+	echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 }
-
-fclose($file);
-$userid = $members;
-for($i=1;$i<count($userid);$i++){
-	$response = $bot->pushMessage($userid[$i], $textMessageBuilder);
-}
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
-
 
 
 
